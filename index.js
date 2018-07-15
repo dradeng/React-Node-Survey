f98ff79e57956a679bdd0cb84abd240d8ef2186d
Draden Gaffney <dsg2dm@virginia.edu>
2018-07-15 16:35:21 -0700

    new auth

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const keys = require('./config/keys');
+const cookieSession = require('cookie-session');
+
+require('./models/User');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
 
-app.get('/', (req,res) => {
-  res.send({ bye: "there"});
-});
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 3600 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
 
 
 const PORT = process.env.PORT || 5000;
