4a26a7fc41c2389a33805f9758e92727d36774cc
Draden Gaffney <dsg2dm@virginia.edu>
2018-08-14 23:50:26 -0700

    dashboard change

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-
+import { Link } from 'react-router-dom';
 
 const Dashboard = () => {
   return (
     <div>
       dashboard
+      <div className="fixed-action-btn">
+        <Link to="/surveys/New" className="btn-floating btn-large red">
+          <i className="material-icons">add</i>
+        </a>
+      </div>
     </div>
   );
 };
