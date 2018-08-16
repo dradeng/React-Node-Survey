bdd0ff7a9ebfb46dcc2222a9e9c74c553c528279
Draden Gaffney <dsg2dm@virginia.edu>
2018-08-16 00:22:30 -0700

    new survey components

---
+++ client/src/components/surveys/SurveyField.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default ({ input, label }) => {
+
+  return (
+    <div>
+      <label>{label}</label>
+      <input {...input} />
+    </div>
+  );
+};
--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,12 +1,46 @@
 import React, { Component } from 'react';
-import { reduxForm } from 'redux-form';
-
+import { reduxForm, Field } from 'redux-form';
+import SurveyField from './SurveyField'
 
 class SurveyForm extends Component {
+  renderFields() {
+    return (
+      <div>
+        <Field
+          type="text"
+          label="Survey Title"
+          name="title"
+          component={SurveyField}
+        />
+        <Field
+          type="text"
+          label="Subject Line"
+          name="title"
+          component={SurveyField}
+        />
+        <Field
+          type="text"
+          label="Email Body"
+          name="title"
+          component={SurveyField}
+        />
+        <Field
+          type="text"
+          label="Recipient"
+          name="title"
+          component={SurveyField}
+        />
+      </div>
+    )
+  }
+
   render() {
     return (
       <div>
-        Survey Form
+        <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+          {this.renderFields()}
+          <button type="submit">Submit</button>
+        </form>
       </div>
     );
   }
