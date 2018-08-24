58b8a89ca3a7f02174005253a4a84427776dbc35
Draden Gaffney <dsg2dm@virginia.edu>
2018-08-24 15:30:54 -0400

    looks

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,37 +1,28 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
 import SurveyField from './SurveyField'
 
+const FIELDS = [
+  { label: 'Survey Title', name: 'title'},
+  { label: 'Subject Line', name: 'subject'},
+  { label: 'Email Body', name: 'body'},
+  {label: 'Recipient List', name: 'emails'}
+];
+
 class SurveyForm extends Component {
   renderFields() {
-    return (
-      <div>
+    return _.map(FIELDS, ({ label, name }) => {
+      return (
         <Field
+        key={name}
+          component={Surveyfield}
           type="text"
-          label="Survey Title"
-          name="title"
-          component={SurveyField}
+          label={label}
+          ame={name}
         />
-        <Field
-          type="text"
-          label="Subject Line"
-          name="title"
-          component={SurveyField}
-        />
-        <Field
-          type="text"
-          label="Email Body"
-          name="title"
-          component={SurveyField}
-        />
-        <Field
-          type="text"
-          label="Recipient"
-          name="title"
-          component={SurveyField}
-        />
-      </div>
-    )
+      );
+    });
   }
 
   render() {
@@ -39,7 +30,10 @@ class SurveyForm extends Component {
       <div>
         <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
           {this.renderFields()}
-          <button type="submit">Submit</button>
+          <button type="submit" className="teal btn-flat right white-text">
+            Next
+            <i className="material-icons right">done</i>
+          </button>
         </form>
       </div>
     );
